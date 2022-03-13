f300b42d6d380076f6b0b225e78924cf7c2aaee6
Yash Eksambekar <yasheksambekar@gmail.com>
2022-03-13 19:36:30 +0530

    Social screen complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:fitness_buddy/app.dart';
+import 'package:fitness_buddy/screens/signin.dart';
+import 'package:fitness_buddy/screens/signup.dart';
+import 'package:fitness_buddy/screens/username.dart';
 import 'package:flutter/material.dart';
 
 void main() {
